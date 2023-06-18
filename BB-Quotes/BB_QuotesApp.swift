19f5fcafd7c7391cfc9b31ce792bcb78d44c55df
ptejas26 <ptejas.patelia@gmail.com>
2023-06-18 13:57:37 -0400

    Initial Commit

---
+++ BB-Quotes/BB_QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BB_QuotesApp.swift
+//  BB-Quotes
+//
+//  Created by Tejas on 2023-06-18.
+//
+
+import SwiftUI
+
+@main
+struct BB_QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BB-Quotes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BB-Quotes
+//
+//  Created by Tejas on 2023-06-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
