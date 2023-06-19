a29466717aab1c7b505df5b91706d8f08f96243d
ptejas26 <ptejas.patelia@gmail.com>
2023-06-18 23:52:49 -0400

    Lesson 7: completed the view

--- BB-Quotes/QuotesView.swift
+++ BB-Quotes/QuotesView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct QuotesView: View {
+    
+//    let viewModel = ViewModel(controller: FetchController())
     var body: some View {
         GeometryReader { geo in
         
@@ -16,7 +18,11 @@ struct QuotesView: View {
                     .resizable()
                     .frame(width: geo.size.width * 2.7, height: geo.size.height * 1.2)
                 VStack {
+                    Spacer()
+                    
                     Text("\"You either run from things, or you face them, Mr. White\"")
+                    // this property will scale down the font size for long text size 
+                        .minimumScaleFactor(0.5)
                         .multilineTextAlignment(.center)
                         .foregroundColor(.white)
                         .padding()
@@ -36,6 +42,22 @@ struct QuotesView: View {
                     }
                     .frame(width: geo.size.width/1.1, height: geo.size.height/1.8)
                     .cornerRadius(40)
+                
+                    Spacer()
+                    
+                    Button() {
+                        
+                    } label: {
+                        Text("Get Random Quotes")
+                            .font(.title)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color(.breadingBadGreen))
+                            .cornerRadius(12)
+                            .shadow(color: Color(.breadBadYellow), radius: 10)
+                    }
+                    
+                    Spacer(minLength: 100)
                 }
                 .frame(width: geo.size.width, height: geo.size.height)
             }
